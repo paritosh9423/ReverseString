3648ae0e94f42c39caac587ad0ed19bab608b530
Paritosh Pradeep <paritosh.9423@gmail.com>
2020-01-16 17:58:53 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    String str = "1 2 3 4 5";
+    char temp ;
+    
+    char[] arr = str.toCharArray();
+    int len = arr.length-1;
+    for(int i=0;i<arr.length/2 ;i++){
+      temp = arr[i];
+      arr[i]=arr[len];
+      arr[len] = temp;
+      len--;
+       System.out.println(arr);
+    }
+    System.out.println(arr);
+
+  }
+}
