ebde06dcbddf8f32c5bdbcdaa572fdacb8dd1a02
Paritosh Pradeep <paritosh.9423@gmail.com>
2020-01-16 17:59:49 +0000

    comments

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ class Main {
     System.out.println("Hello world!");
     String str = "1 2 3 4 5";
     char temp ;
-    
+    //Reverse without extra char array.
     char[] arr = str.toCharArray();
     int len = arr.length-1;
     for(int i=0;i<arr.length/2 ;i++){
